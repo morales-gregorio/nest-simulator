2a9e0a257fcd96ab6327e4487b2bf0161efc6f65
Michael Denker <m.denker@fz-juelich.de>
2017-01-27 18:31:38 +0100

    Provide printouts of all NEST annotations

--- pynest/nest/example/neo_bridge.py
+++ pynest/nest/example/neo_bridge.py
@@ -1,8 +1,11 @@
-import elephant
-import neo
+"""
+Module to convert NEST output and simulation metadata into the hierarchical Neo
+object model.
+"""
 import numpy as np
 import quantities as pq
 
+import neo
 import nest
 
 
@@ -20,21 +23,43 @@ def segment_from_device(device):
 
 
 def from_device(device):
+    """
+    Extracts data from a NEST device and stores it in a Neo SpikeTrain or
+    AnalogSignal structure.
+    
+    Parameters:
+    -----------
+    
+    Returns:
+    --------
+    
+    """
+    
+    # Determine whether the device saved to memory or to file
     to_memory = nest.GetStatus(device, 'to_memory')
     to_file = nest.GetStatus(device, 'to_file')
 
     if not to_memory and not to_file:
-        raise ValueError('sadasdasd')
+        raise ValueError('pynest.neo_bridge: Device did not output to memory.')
 
+    # Get data from memory
     if to_memory:
         events = nest.GetStatus(device, 'events')[0]
         senders = events['senders']
         times = events['times']
 
+        # For each sender, create a corresponding SpikeTrain object
         st = []
         for gid in np.unique(senders):
-            x = neo.SpikeTrain(times[senders == gid] * pq.ms, t_start=0. * pq.ms, t_stop=nest.GetKernelStatus('time') * pq.ms)
-            x.annotate(gid=gid, sampling_period=nest.GetKernelStatus('resolution'))
+            x = neo.SpikeTrain(
+                times[senders == gid] * pq.ms, 
+                t_start=0. * pq.ms, 
+                t_stop=nest.GetKernelStatus('time') * pq.ms)
+            
+            x.annotate(
+                gid=gid, 
+                sampling_period=nest.GetKernelStatus('resolution'))
+            
             st.append(x)
 
         return st
--- pynest/nest/example/simple_network.py
+++ pynest/nest/example/simple_network.py
@@ -1,23 +1,32 @@
-import sys
-print sys.path
-
-import elephant
-import neo
 import matplotlib.pyplot as plt
+
 import numpy as np
 import quantities as pq
 
-import nest
+import elephant
+import neo
+
 from neo_bridge import block_from_device, segment_from_device, from_device
+import nest
 
 
 nest.ResetKernel()
 # nest.SetKernelStatus({'overwrite_files': True})
 nest.SetKernelStatus({'resolution': 0.01})
 
+print "Status of NEST Kernel before simulation"
+ks_before = nest.GetKernelStatus()
+for s in ks_before:
+    print(s, ": ", ks_before[s])
+print()
+
 neuron = nest.Create('iaf_psc_delta', 2)
 spikedetector = nest.Create('spike_detector')
 poisson = nest.Create('poisson_generator', 1, {'rate': 16000.})
+
+print("IDs of devices")
+print neuron, spikedetector, poisson
+print()
 
 # nest.SetStatus(neuron, {'I_e': 300.})
 # nest.SetStatus(spikedetector, {'to_file': True, 'to_memory': False})
@@ -27,16 +36,37 @@ nest.Connect(poisson, neuron)
 
 nest.Simulate(500.)
 
-my_block = block_from_device(spikedetector)
-# spike_trains = from_device(spikedetector)
+print("Changes of status of NEST Kernel after simulation")
+ks_after = nest.GetKernelStatus()
+for s in ks_after:
+    if ks_before[s] != ks_after[s]:
+        print(s, ": ", ks_before[s], "->", ks_after[s])
+print()
 
-spike_trains = my_block.filter(gid=2, object=neo.SpikeTrain)
+spike_trains = from_device(spikedetector)
+
+print("Status of Neuron")
+neus = nest.GetStatus(neuron)[0]
+for s in neus:
+    print(s, ": ", neus[s])
+print()
+
+
+print("Status of Spike Detector")
+spds = nest.GetStatus(spikedetector)[0]
+for s in spds:
+    print(s, ": ", spds[s])
+print()
+
+
+# my_block = block_from_device(spikedetector)
+# spike_trains = my_block.filter(gid=2, object=neo.SpikeTrain)
 
 # plt.plot(np.arange(spike_trains[0].t_start, spike_trains, 0.1
 
-plt_time_unit="ms"
-ir = elephant.statistics.instantaneous_rate(spike_trains[0], spike_trains[0].annotations['sampling_period'] * pq.ms, kernel=elephant.kernels.GaussianKernel(sigma=30. * pq.ms, invert=False))
-plt.plot(ir.times.rescale(plt_time_unit),ir.rescale("Hz"))
-plt.xlabel(plt_time_unit)
-plt.ylabel('Hz')
-plt.show()
+# plt_time_unit="ms"
+# ir = elephant.statistics.instantaneous_rate(spike_trains[0], spike_trains[0].annotations['sampling_period'] * pq.ms, kernel=elephant.kernels.GaussianKernel(sigma=30. * pq.ms, invert=False))
+# plt.plot(ir.times.rescale(plt_time_unit),ir.rescale("Hz"))
+# plt.xlabel(plt_time_unit)
+# plt.ylabel('Hz')
+# plt.show()
